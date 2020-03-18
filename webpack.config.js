e930bb03a78fcabd080ff11cc319dbdb5c595d5a
toshifumiimanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-03-18 21:22:14 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+module.exports = (env, argv) => {
+  const MODE = argv.mode || 'development';
+  const enabledSourceMap = MODE === 'development';
+  const config = {
+    mode: MODE,
+    entry: './src/js/entry.js',
+    output: {
+      path: `${__dirname}/htdocs/js`,
+      filename: 'main.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                url: false,
+                // sorceMap: enabledSourceMap,
+                importLoaders: 2
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sorceMap: enabledSourceMap,
+                plugins: [
+                  require('autoprefixer')({
+                    grid: 'autoplace'
+                  })
+                ]
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sorceMap: enabledSourceMap
+              }
+            }
+          ]
+        },
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env'
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    devServer: {
+      contentBase: 'htdocs',
+      open: false
+    }
+  }
+
+  return config;
+};
