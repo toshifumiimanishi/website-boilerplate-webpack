ece410cb6ef0cbadb05134037eb53a718b997601
toshifumiimanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-05-19 16:36:01 +0900

    :sparkles: Add: URL をクリップボードにコピーする機能開発

---
+++ src/js/modules/utils/clipboard.ts
@@ -0,0 +1,20 @@
+export const clipboard = (() => {
+  const _copyURL = () => {
+    const url = location.href
+    const tempElm = document.createElement('div')
+    tempElm.appendChild(document.createElement('pre')).textContent = url
+
+    const style = tempElm.style
+    style.position = 'fixed'
+    style.left = '-100%'
+
+    document.body.appendChild(tempElm)
+    document.getSelection().selectAllChildren(tempElm)
+    document.execCommand('copy')
+    document.body.removeChild(tempElm)
+  }
+
+  return {
+    copyURL: _copyURL,
+  }
+})()
